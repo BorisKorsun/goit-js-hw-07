ea3b133a032c04ce102ec9eca0b497faf8f7feca
BorisKorsun <91095836+BorisKorsun@users.noreply.github.com>
2022-03-27 00:38:54 +0200

    render

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryRef: document.querySelector('.gallery')
+}
+
+function createGalleryList(array) {
+    return array
+    .map(({ preview, original, description }) => {
+        const div = document.createElement('div');
+        div.classList.add('gallery__item');
+
+        const link = document.createElement('a');
+        link.classList.add('gallery__link');
+        link.href = original;
+        div.appendChild(link);
+
+        const image = document.createElement('img');
+        image.classList.add('gallery__image');
+        image.src = preview;
+        image.dataset.source = original;
+        image.alt = description;
+        link.appendChild(image);
+
+        refs.galleryRef.appendChild(div)
+    })
+};
+
+createGalleryList(galleryItems);
+
+refs.galleryRef.addEventListener('click', onGalleryImageClick)
+
+function onGalleryImageClick(e) {
+    e.preventDefault()
+
+    const image = e.target
+
+    if (image.nodeName !== 'IMG') {
+        return
+    }
+    console.log(image.dataset.source)
+    console.log('function completed')
+};
