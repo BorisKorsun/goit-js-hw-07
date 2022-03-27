afee71790de6685a11740b94c996deb02d7439c8
BorisKorsun <91095836+BorisKorsun@users.noreply.github.com>
2022-03-28 00:48:15 +0300

    task 01, task 02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,43 +2,75 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const refs = {
-    galleryRef: document.querySelector('.gallery')
-}
-
-function createGalleryList(array) {
-    return array
-    .map(({ preview, original, description }) => {
-        const div = document.createElement('div');
-        div.classList.add('gallery__item');
-
-        const link = document.createElement('a');
-        link.classList.add('gallery__link');
-        link.href = original;
-        div.appendChild(link);
-
-        const image = document.createElement('img');
-        image.classList.add('gallery__image');
-        image.src = preview;
-        image.dataset.source = original;
-        image.alt = description;
-        link.appendChild(image);
-
-        refs.galleryRef.appendChild(div)
-    })
+    galleryRef: document.querySelector('.gallery'),
 };
 
-createGalleryList(galleryItems);
+function createGalleryList(arr) {
+    return arr
+    .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+        </div>
+        `;
+    }).join('');
+};
 
-refs.galleryRef.addEventListener('click', onGalleryImageClick)
+const galleryMarkup = createGalleryList(galleryItems);
+
+refs.galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+refs.galleryRef.addEventListener('click', onGalleryImageClick);
 
 function onGalleryImageClick(e) {
     e.preventDefault()
 
-    const image = e.target
 
-    if (image.nodeName !== 'IMG') {
-        return
-    }
-    console.log(image.dataset.source)
-    console.log('function completed')
+    const image = e.target;
+    
+    if(image.nodeName !== 'IMG') {
+        return;
+    };
+
+    const modal = basicLightbox.create(`
+        <img src="${image.dataset.source}" width="800" height="600">
+    `);
+
+    modal.show();
+    console.log('function completed');
 };
+
+
+// function createGalleryList(array) {
+//     return array
+//     .map(({ preview, original, description }) => {
+//         const div = document.createElement('div');
+//         div.classList.add('gallery__item');
+
+//         const link = document.createElement('a');
+//         link.classList.add('gallery__link');
+//         link.href = original;
+//         div.appendChild(link);
+
+//         const image = document.createElement('img');
+//         image.classList.add('gallery__image');
+//         image.src = preview;
+//         image.dataset.source = original;
+//         image.alt = description;
+//         link.appendChild(image);
+
+//         refs.galleryRef.appendChild(div)
+//     })
+// };
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryRef: document.querySelector('.gallery'),
+};
+
+function createGalleryList(arr) {
+    return arr
+    .map(({ preview, original, description }) => {
+        return `
+        <li>
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>
+        `;
+    }).join('');
+};
+
+const galleryMarkup = createGalleryList(galleryItems);
+
+refs.galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
